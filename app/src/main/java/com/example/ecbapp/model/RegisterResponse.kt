78c6a5d264a1315c9238ee7ead71b82ca55a926f
Claive Justin Barrientos <claivejustinbarrientos12@gmail.com>
2025-04-24 00:58:17 +0800

    Merge remote-tracking branch 'origin/android' into feature-nottricko-PepitoJP3
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/example/ecbapp/model/RegisterResponse.kt
@@ -0,0 +1,6 @@
+package com.example.ecbapp.model
+
+data class RegisterResponse(
+    val message: String,
+    val success: Boolean
+)
---
+++ app/src/main/java/com/example/ecbapp/model/UserProfile.kt
@@ -0,0 +1,9 @@
+package com.example.ecbapp.model
+
+data class UserProfile(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val address: String,
+    val contactNo: String
+)
