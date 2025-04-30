4148832120429d8070bf46aaccb9c1e7ecc5b95e
jahnluuu <131157302+jahnluuu@users.noreply.github.com>
2025-05-01 01:48:09 +0800

    for frontend deployment

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  //baseURL: 'http://localhost:8080',
+  baseURL: 'https://ecb-backend.onrender.com',
   withCredentials: true, // In case you also want to send cookies
 });
 
